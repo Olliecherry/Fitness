67aae420ffa56e7b110a49b2e3a38a9c993b109d
Olga Peganova <volha25121987@gmail.com>
2023-06-30 17:06:56 +0700

    accord

--- source/js/vendor/swiper.js
+++ source/js/vendor/swiper.js
@@ -1,3 +1,22 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+
+
 const swiper = () => {
   /**
    * Swiper 7.4.1
