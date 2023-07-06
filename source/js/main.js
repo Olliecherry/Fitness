69138cacabfae51e0be8a966b63ee0bbef0abff0
Olga Peganova <volha25121987@gmail.com>
2023-07-06 18:13:21 +0700

    swipe

--- source/js/main.js
+++ source/js/main.js
@@ -61,3 +61,19 @@ btnStop.addEventListener('click', function () {
 // breakpointChecker();
 
 // используйте .closest(el)
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
